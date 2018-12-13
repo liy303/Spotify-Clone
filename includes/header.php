6858b07d4462a33d45d0f3e404a694408b57df70
lin yuan <lyuan8733@gmail.com>
2018-12-13 00:05:20 -0700

    all spotify clone file

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<?php
+include("includes/config.php");
+include("includes/classes/User.php");
+include("includes/classes/Artist.php");
+include("includes/classes/Album.php");
+include("includes/classes/Song.php");
+include("includes/classes/Playlist.php");
+
+// session_destroy(); <- only include this when you want to log out mannually.
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = new User($con, $_SESSION['userLoggedIn']);
+    $username = $userLoggedIn->getUsername();
+	echo "<script>userLoggedIn = '$username';</script>";
+} else {
+    header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src='assets/js/script.js'></script>
+    <title>Document</title>
+</head>
+<body>
+    <div id='mainContainer'>
+         <div id='topContainer'>
+           <?php include("includes/navBarContainer.php"); ?> 
+
+           <div id='mainViewContainer'>
+               <div id='mainContent'>
